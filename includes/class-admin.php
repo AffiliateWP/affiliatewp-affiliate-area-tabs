4fcc05582092bb0e72d292ed62e20fae5d23fad0
Andrew Munro <andrew@amdrew.com>
2017-11-21 19:17:41 +1300

    Improved sanitization
    + cleaned up script loading
    + reorganized methods
    + set up text localization
    + cleaned up tabs_list()

--- includes/class-admin.php
+++ includes/class-admin.php
@@ -4,115 +4,15 @@ class AffiliateWP_Affiliate_Area_Tabs_Admin {
 
 	public function __construct() {
 		add_filter( 'affwp_settings_tabs', array( $this, 'settings_tab' ) );
-		add_filter( 'affwp_settings_affiliate_area_tabs_sanitize', array( $this, 'sanitize_tabs' ), 10, 1 );
 		add_action( 'admin_init', array( $this, 'register_settings' ) );
-		add_action( 'admin_enqueue_scripts', array( $this, 'scripts' ), 100 );
-		add_action( 'affiliate_area_tabs_tab_row', array( $this, 'render_tab_row' ), 10, 6 );
+		add_action( 'admin_enqueue_scripts', array( $this, 'admin_scripts' ), 100 );
+		add_action( 'affiliate_area_tabs_tab_row', array( $this, 'render_tab_row' ), 10, 3 );
+		add_filter( 'pre_update_option_affwp_settings', array( $this, 'pre_update_option' ), 10, 2 );
 	}
 
-	/**
-	 * Sanitize tabs
-	 *
-	 * @access public
-	 * @since 1.2
-	 * 
-	 * @return array $input
-	 */
-	public function sanitize_tabs( $input ) {
-
-		if ( ! empty($input['affiliate_area_tabs'] ) ) {
-
-			// The tabs currently stored in the DB.
-			$affiliate_area_tabs = affiliate_wp()->settings->get( 'affiliate_area_tabs' );
-
-			// Sanitize each tab.
-			foreach ( $input['affiliate_area_tabs'] as $key => $tab ) {
-				
-				// Tab's must have both a title and id assigned.
-				if ( empty( $tab['title'] ) || ! isset( $tab['id'] ) ) {
-					
-					// Unset the tab
-					unset( $input['affiliate_area_tabs'][$key] );
-					
-					// Skip to the next tab.
-					continue;
-				}
-
-				// Create an initial tab slug for custom tabs (core tabs already have a slug).
-				if ( empty( $tab['slug'] ) ) {
-
-					// Create a slug from the tab's title
-					$new_slug = affiliatewp_affiliate_area_tabs()->make_slug( $tab['title'] );
-
-					// Check if the new tab slug already exists in AffiliateWP's list of tabs
-					if ( array_key_exists( $new_slug, affwp_get_affiliate_area_tabs() ) ) {
-						// If so, unset the tab. 
-						unset( $input['affiliate_area_tabs'][$key] );
-
-						// Skip to the next tab.
-						continue;
-
-					} else {
-						// It's a unique slug, store it with the tab
-						$input['affiliate_area_tabs'][$key]['slug'] = $new_slug;
-					}
-
-				}
-
-				// Existing custom tab.
-				if ( $this->is_custom_tab( $tab['slug'] ) ) {
-
-					// Create a slug.
-					$new_slug = affiliatewp_affiliate_area_tabs()->make_slug( $tab['title'] );
-
-					// Update a custom tab's slug when the custom tab's title is changed, and it doesn't already exist.
-					// If the new title mimicks an existing tab, it will simply keep the previous tab slug, rather than unsetting it.
-					if ( $tab['title'] !== $affiliate_area_tabs[$key]['title'] && ! array_key_exists( $new_slug, affwp_get_affiliate_area_tabs() ) ) {
-						$input['affiliate_area_tabs'][$key]['slug'] = $new_slug;
-					}
-				
-				}
-				
-			}
-
-		}
-
-		return $input;
-	}
 
 	/**
-	 * Scripts
-	 *
-	 * @access public
-	 * @since 1.2
-	 */
-	public function scripts() {
-
-		// Admin CSS file.
-		$screen = get_current_screen();
-
-		$js_dir  = AFFWP_AAT_PLUGIN_URL . 'assets/js/';
-		
-		// Use minified libraries if SCRIPT_DEBUG is turned off
-		$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
-		
-		$admin_deps = array();
-
-		wp_register_style( 'aat-admin', AFFWP_AAT_PLUGIN_URL . 'assets/css/admin' . $suffix . '.css', array( 'dashicons' ), AFFWP_AAT_VERSION );
-		wp_register_script( 'aat-admin-scripts', $js_dir . 'admin-scripts' . $suffix . '.js', $admin_deps, AFFWP_AAT_VERSION, false );
-
-		if (
-			$screen->id === 'affiliates_page_affiliate-wp-settings' &&
-			isset( $_GET['tab'] ) && $_GET['tab'] === 'affiliate_area_tabs'
-		) {
-			wp_enqueue_style( 'aat-admin' );
-			wp_enqueue_script( 'aat-admin-scripts' );
-		}
-
-	}
-
-	/**
-	 * Register the new settings tab
+	 * Register the new settings tab.
 	 *
 	 * @access public
 	 * @since 1.0.0
@@ -124,38 +24,14 @@ class AffiliateWP_Affiliate_Area_Tabs_Admin {
 	}
 
 	/**
-	 * Get a list of all tabs
-	 *
-	 * @access public
-	 * @since 1.1.6
-	 * @return array $tabs The tabs to output on the Affiliates -> Settings -> Affiliate Area Tabs page.
-	 */
-	public function get_tabs() {
-
-		$tabs = array();
-
-		if ( function_exists( 'affwp_get_affiliate_area_tabs' ) ) {
-			$tabs = affwp_get_affiliate_area_tabs();
-		} else {
-			/**
-			 * If a previous version of AffiliateWP is being used, output the
-			 * hard-coded tabs as before.
-			 */
-			$tabs = $this->default_tabs();
-		}
-
-		return $tabs;
-
-	}
-
-	/**
 	 * Register our settings
 	 *
 	 * @access public
 	 * @since 1.0.0
+	 * 
 	 * @return array
 	 */
-	public function register_settings( ) {
+	public function register_settings() {
 
 		add_settings_field(
 			'affwp_settings[affiliate_area_tabs_list]',
@@ -172,135 +48,176 @@ class AffiliateWP_Affiliate_Area_Tabs_Admin {
 	 *
 	 * @access private
 	 * @since 1.2
+	 * 
 	 * @return string
 	 */
 	private function expand_collapse_tabs() {
 		ob_start();
+
+		$expand_text   = __( 'Expand all tabs', 'affiliatewp-affiliate-area-tabs' );
+		$collapse_text = __( 'Collapse all tabs', 'affiliatewp-affiliate-area-tabs' );
 		?>
 		<p>
-			<a href="#" class="aat-hide-show-tabs" data-text-swap="<?php _e( 'Collapse all tabs', 'affiliatewp-affiliate-area-tabs' );?>" data-text-original="<?php _e( 'Expand all tabs', 'affiliatewp-affiliate-area-tabs' );?>"><?php _e( 'Expand all tabs', 'affiliatewp-affiliate-area-tabs' );?></a>
+			<a href="#" class="aat-hide-show-tabs" data-text-swap="<?php echo $collapse_text; ?>" data-text-original="<?php echo $expand_text; ?>"><?php echo $expand_text; ?></a>
 		</p>
 		<?php
 		return ob_get_clean();
 	}
 
 	/**
-	 * Returns an array of pages without the Affiliate Area.
+	 * Sanitize tabs
+	 *
+	 * @access public
+	 * @since 1.2
+	 * @param $new_value array of new values
+	 * @param $old_value array of old values
 	 * 
-	 * @since 1.1.2
+	 * @return array $new_value
 	 */
-	private function get_pages() {
+	public function pre_update_option( $new_value, $old_value ) {
 
-		$pages             = affwp_get_pages();
-		$affiliate_area_id = function_exists( 'affwp_get_affiliate_area_page_id' ) ? affwp_get_affiliate_area_page_id() : affiliate_wp()->settings->get( 'affiliates_page' );
+		// Loop through tabs.
+		foreach ( $new_value['affiliate_area_tabs'] as $key => $tab ) {
+			
+			// Skip sanitization on any non-custom tab.
+			if ( 0 === $tab['id'] ) {
+				continue;
+			}
 
-		if ( ! empty( $pages[ $affiliate_area_id ] ) ) {
-			unset( $pages[ $affiliate_area_id ] );
+			// Tab's must have both a title and id assigned.
+			if ( empty( $tab['title'] ) || ! isset( $tab['id'] ) ) {
+				
+				// Unset the tab
+				unset( $new_value['affiliate_area_tabs'][$key] );
+				
+				// Skip to the next tab.
+				continue;
+			}
+
+			// Create an initial tab slug for custom tabs (core tabs already have a slug).
+			if ( empty( $tab['slug'] ) ) {
+				
+				// Create a slug from the tab's title
+				$new_value['affiliate_area_tabs'][$key]['slug'] = affiliatewp_affiliate_area_tabs()->functions->make_slug( $tab['title'] );
+
+			}
+
+			// Force the tab ID to be an integer.
+			$new_value['affiliate_area_tabs'][$key]['id'] = (int) $new_value['affiliate_area_tabs'][$key]['id'];
+			
+
+			// Determine if the tab is a custom tab.
+			if ( affiliatewp_affiliate_area_tabs()->functions->is_custom_tab( $tab['slug'] ) ) {
+
+				/**
+				 * Loop through the old values
+				 * 
+				 * This is neccessary since custom tabs could have moved position via the admin interface.
+				 * 
+				 * First we check if the custom tab exists in the old values.
+				 * If so, we then check its title. If the title changed, we attempt
+				 * To update its tab slug.
+				 */
+				foreach ( $old_value['affiliate_area_tabs'] as $old_key => $old_tab ) {
+					
+					// Found the custom slug, must be the same tab.
+					if ( $old_tab['slug'] === $tab['slug'] ) {
+						
+						// Check to see if the tab's title was changed.
+						if ( $old_tab['title'] !== $tab['title'] ) {
+							// Create a new slug.
+							$new_slug = affiliatewp_affiliate_area_tabs()->functions->make_slug( $tab['title'] );
+
+							// Check that the slug isn't already in-use.
+							if ( ! array_key_exists( $new_slug, affwp_get_affiliate_area_tabs() ) ) {
+								// Slug isn't being used, use the new slug.
+								$new_value['affiliate_area_tabs'][$key]['slug'] = $new_slug;
+							}
+
+							// If the new slug is already in-use, the slug will not change, and remain as its previous value.
+						
+						}
+
+					}
+				}
+
+			}
+
 		}
 
-		return $pages;
+		return $new_value;
+
+	}
+
+	/**
+	 * Admin scripts.
+	 *
+	 * @access public
+	 * @since 1.2
+	 */
+	public function admin_scripts() {
+
+		// Admin CSS file.
+		$screen = get_current_screen();
+
+		// Use minified libraries if SCRIPT_DEBUG is set to false.
+		$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
+
+		// Register scripts.
+		wp_register_style( 'aat-admin', AFFWP_AAT_PLUGIN_URL . 'assets/css/admin' . $suffix . '.css', array( 'dashicons' ), AFFWP_AAT_VERSION );
+		wp_register_script( 'aat-admin-scripts', AFFWP_AAT_PLUGIN_URL . 'assets/js/admin-scripts' . $suffix . '.js',  array(), AFFWP_AAT_VERSION, false );
+
+		// Enqueue scripts.
+		if ( $screen->id === 'affiliates_page_affiliate-wp-settings' && isset( $_GET['tab'] ) && $_GET['tab'] === 'affiliate_area_tabs' ) {
+			wp_enqueue_style( 'aat-admin' );
+			wp_enqueue_script( 'aat-admin-scripts' );
+		}
+
 	}
 
 	/**
 	 * Render the tabs list
+	 * 
 	 * @since 1.0.0
 	 */
 	public function tabs_list() {
 		
-			$tabs  = $this->get_tabs();
-			$count = count( $tabs );
-
-			$i = 0;
-			?>
-			<div class="widefat aat_repeatable_table">
-			
-				<div class="aat-repeatables-wrap">
-				<?php 
-					foreach ( $tabs as $tab_slug => $tab_title ) : $i++; ?>
-					
-					<?php
-					$key = $i;
-					$args = array();
-					$post_id = '';
-					$index = $key;
-				?>
-
+		$tabs  = affiliatewp_affiliate_area_tabs()->functions->get_tabs();
+		$count = count( $tabs );
+		$key   = 0;
+		?>
+		
+		<div class="widefat aat_repeatable_table">
+		
+			<div class="aat-repeatables-wrap">
+				<?php foreach ( $tabs as $tab_slug => $tab_title ) : $key++; ?>
 					<div class="aat_repeatable_row" data-key="<?php echo esc_attr( $key ); ?>">
-						<?php do_action( 'affiliate_area_tabs_tab_row', $key, $args, $post_id, $index, $tab_slug, $tab_title ); ?>
+						<?php do_action( 'affiliate_area_tabs_tab_row', $key, $tab_slug, $tab_title ); ?>
 					</div>
-					
-					<?php endforeach; ?>
+				<?php endforeach; ?>
 
-					<div class="aat-add-repeatable-row">
-						<button class="button-secondary aat-add-repeatable"><?php _e( 'Add New Tab', 'affiliatewp-affiliate-area-tabs' ); ?></button>
-					</div>
-
+				<div class="aat-add-repeatable-row">
+					<button class="button-secondary aat-add-repeatable"><?php _e( 'Add New Tab', 'affiliatewp-affiliate-area-tabs' ); ?></button>
 				</div>
-			</div>
 
+			</div>
+		</div>
 	<?php
 	}
 	
 	/**
-	 * Tab notice.
-	 * Useful for add-ons to let the end-user know where the tab was added from.
-	 * 
-	 * @since 1.2
-	 * @param string $tab_slug The slug of the tab
-	 * 
-	 * @return string $notice The notice to show on the tab
-	 */
-	public function tab_notice( $tab_slug ) {
-
-		switch ( $tab_slug ) {
-
-			case $this->is_default_tab( $tab_slug ):
-				$notice = __( 'This is a default AffiliateWP tab.', 'affiliatewp-affiliate-area-tabs' );
-				break;
-			
-			case 'order-details':
-				$notice = __( 'This tab has been added from the <em>Order Details For Affiliates</em> add-on.', 'affiliatewp-affiliate-area-tabs' );
-				break;
-
-			case 'direct-links':
-				$notice = __( 'This tab has been added from the <em>Direct Link Tracking</em> add-on.', 'affiliatewp-affiliate-area-tabs' );
-				break;
-
-			case 'coupons':
-				$notice = __( 'This tab has been added from the <em>Show Affiliate Coupons</em> add-on.', 'affiliatewp-affiliate-area-tabs' );
-				break;
-
-			default:
-				$notice = '';
-				break;
-		}
-
-		return apply_filters( 'affwp_aat_tab_notice', $notice, $tab_slug );
-
-	}
-
-	/**
 	 * Individual Tab Row
 	 *
 	 * Used to output a row for each tab.
-	 * Can be called directly, or attached to an action.
 	 *
 	 * @since 1.2
 	 *
 	 * @param       $key
-	 * @param array $args
-	 * @param       $post_id
+	 * @param string $tab_slug
+	 * @param string $tab_title
 	 */
-	public function render_tab_row( $key, $args = array(), $post_id, $index, $tab_slug, $tab_title ) {
-	
-		$defaults = array(
-			'name'   => null,
-			'amount' => null
-		);
-
-		$args = wp_parse_args( $args, $defaults );
-
-	?>
+	public function render_tab_row( $key, $tab_slug, $tab_title ) {
+		?>
 
 		<div class="aat-draghandle-anchor">
 			<span class="dashicons dashicons-move" title="<?php _e( 'Click and drag to re-order', 'affiliatewp-affiliate-area-tabs' ); ?>"></span>
@@ -317,7 +234,7 @@ class AffiliateWP_Affiliate_Area_Tabs_Admin {
 			
 			<div class="aat-repeatable-row-standard-fields" style="display: none;">
 
-				<?php if ( $tab_notice = $this->tab_notice( $tab_slug ) ) : ?>
+				<?php if ( $tab_notice = $this->tab_notices( $tab_slug ) ) : ?>
 					<p class="aat-tab-default"><?php echo $tab_notice; ?></p>
 				<?php endif; ?>
 
@@ -326,7 +243,7 @@ class AffiliateWP_Affiliate_Area_Tabs_Admin {
 				/**
 				 * Hide a field if it's not a custom tab.
 				 */
-				$hidden = ! $this->is_custom_tab( $tab_slug ) ? ' style="display: none;"' : '';
+				$hidden = ! affiliatewp_affiliate_area_tabs()->functions->is_custom_tab( $tab_slug ) ? ' style="display: none;"' : '';
 
 				/**
 				 * Tab title.
@@ -360,7 +277,8 @@ class AffiliateWP_Affiliate_Area_Tabs_Admin {
 					<span class="description"><?php _e( 'Select which page will be used for the tab\'s content. This page will be blocked for non-affiliates.', 'affiliatewp-affiliate-area-tabs' ); ?></span>
 						
 					<?php
-					$pages = $this->get_pages();
+
+					$pages = affiliatewp_affiliate_area_tabs()->functions->get_pages();
 					$tabs  = affiliate_wp()->settings->get( 'affiliate_area_tabs', array() );
 					?>
 					<select id="affwp_settings[affiliate_area_tabs][<?php echo $key; ?>][id]" class="widefat" name="affwp_settings[affiliate_area_tabs][<?php echo $key; ?>][id]">
@@ -389,7 +307,7 @@ class AffiliateWP_Affiliate_Area_Tabs_Admin {
 				 * 
 				 * @since 1.2
 				 */
-				if ( $this->is_custom_tab( $tab_slug ) ) : ?>
+				if ( affiliatewp_affiliate_area_tabs()->functions->is_custom_tab( $tab_slug ) ) : ?>
 				<p><a href="#" class="aat_remove_repeatable"><?php _e( 'Delete tab', 'affiliatewp-affiliate-area-tabs' ); ?></a></p>
 				<?php endif; ?>
 
@@ -400,76 +318,40 @@ class AffiliateWP_Affiliate_Area_Tabs_Admin {
 	}
 
 	/**
-	 * Determine if the tab is a custom tab or not.
-	 * A custom tab is one that has been added using the "Add New Tab" button.
+	 * Tab notice.
+	 * Useful for add-ons to let the end-user know where the tab was added from.
 	 * 
 	 * @since 1.2
-	 * @uses get_custom_tab_slugs()
+	 * @param string $tab_slug The slug of the tab
 	 * 
-	 * @return boolean True if the tab is a custom tab, false otherwise.
+	 * @return string $notice The notice to show on the tab
 	 */
-	private function is_custom_tab( $tab_slug = '' ) {
-		return in_array( $tab_slug, $this->get_custom_tab_slugs() );
-	}
+	public function tab_notices( $tab_slug ) {
 
-	/**
-	 * Get custom tab slugs
-	 * 
-	 * @since 1.2
-	 * 
-	 * @return array $custom_tab_slugs Array of custom tab slugs
-	 */
-	private function get_custom_tab_slugs() {
+		switch ( $tab_slug ) {
 
-		$tabs = affiliate_wp()->settings->get( 'affiliate_area_tabs', array() );
+			case affiliatewp_affiliate_area_tabs()->functions->is_default_tab( $tab_slug ):
+				$notice = __( 'This is a default AffiliateWP tab.', 'affiliatewp-affiliate-area-tabs' );
+				break;
+			
+			case 'order-details':
+				$notice = __( 'This tab has been added from the <em>Order Details For Affiliates</em> add-on.', 'affiliatewp-affiliate-area-tabs' );
+				break;
 
-		$custom_tab_slugs = array();
+			case 'direct-links':
+				$notice = __( 'This tab has been added from the <em>Direct Link Tracking</em> add-on.', 'affiliatewp-affiliate-area-tabs' );
+				break;
 
-		if ( $tabs ) {
-			foreach( $tabs as $tab_array ) {
-				// Custom tabs have a page ID set.
-				if ( $tab_array['id'] !== '0' ) {
-					$custom_tab_slugs[] = $tab_array['slug'];
-				}
-			}
+			case 'coupons':
+				$notice = __( 'This tab has been added from the <em>Show Affiliate Coupons</em> add-on.', 'affiliatewp-affiliate-area-tabs' );
+				break;
+
+			default:
+				$notice = '';
+				break;
 		}
 
-		return $custom_tab_slugs;
-
-	}
-
-	/**
-	 * Determine if the tab is a default tab or not.
-	 * 
-	 * @since 1.2
-	 * 
-	 * @return boolean True if tab is a default tab, false otherwise.
-	 */
-	public function is_default_tab( $tab_slug ) {
-		return array_key_exists( $tab_slug, $this->default_tabs() );
-	}
-
-	/**
-	 * Holds an array of the default tabs added by AffiliateWP.
-	 * 
-	 * @since 1.2
-	 * 
-	 * @return array $default_tabs Array of default tabs.
-	 */
-	public function default_tabs() {
-
-		$default_tabs = array(
-			'urls'      => __( 'Affiliate URLs', 'affiliatewp-affiliate-area-tabs' ),
-			'stats'     => __( 'Statistics', 'affiliatewp-affiliate-area-tabs' ),
-			'graphs'    => __( 'Graphs', 'affiliatewp-affiliate-area-tabs' ),
-			'referrals' => __( 'Referrals', 'affiliatewp-affiliate-area-tabs' ),
-			'payouts'   => __( 'Payouts', 'affiliatewp-affiliate-area-tabs' ),
-			'visits'    => __( 'Visits', 'affiliatewp-affiliate-area-tabs' ),
-			'creatives' => __( 'Creatives', 'affiliatewp-affiliate-area-tabs' ),
-			'settings'  => __( 'Settings', 'affiliatewp-affiliate-area-tabs' )
-		);
-
-		return $default_tabs;
+		return apply_filters( 'affwp_aat_tab_notices', $notice, $tab_slug );
 
 	}
 
