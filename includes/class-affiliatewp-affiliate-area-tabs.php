aea58ce150231efa061e311bc0522e413c29068e
dbeja <david.beja@gmail.com>
2021-06-07 10:35:55 +0100

    Code styline and docblocks improvements. #98

--- includes/class-affiliatewp-affiliate-area-tabs.php
+++ includes/class-affiliatewp-affiliate-area-tabs.php
@@ -9,7 +9,7 @@
  * @since       1.3
  */
 
-// Exit if accessed directly
+// Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 if ( ! class_exists( 'AffiliateWP_Affiliate_Area_Tabs' ) ) {
@@ -39,6 +39,7 @@ if ( ! class_exists( 'AffiliateWP_Affiliate_Area_Tabs' ) ) {
 		 * The version number of AffiliateWP
 		 *
 		 * @since 1.0
+		 * @var   string
 		 */
 		private $version = '1.2.9';
 
@@ -98,7 +99,7 @@ if ( ! class_exists( 'AffiliateWP_Affiliate_Area_Tabs' ) ) {
 		 * @return void
 		 */
 		public function __clone() {
-			// Cloning instances of the class is forbidden
+			// Cloning instances of the class is forbidden.
 			_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'affiliatewp-affiliate-area-tabs' ), '1.0' );
 		}
 
@@ -110,7 +111,7 @@ if ( ! class_exists( 'AffiliateWP_Affiliate_Area_Tabs' ) ) {
 		 * @return void
 		 */
 		public function __wakeup() {
-			// Unserializing instances of the class is forbidden
+			// Unserializing instances of the class is forbidden.
 			_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'affiliatewp-affiliate-area-tabs' ), '1.0' );
 		}
 
@@ -143,22 +144,22 @@ if ( ! class_exists( 'AffiliateWP_Affiliate_Area_Tabs' ) ) {
 		 * @return void
 		 */
 		private function setup_constants() {
-			// Plugin version
+			// Plugin version.
 			if ( ! defined( 'AFFWP_AAT_VERSION' ) ) {
 				define( 'AFFWP_AAT_VERSION', $this->version );
 			}
 
-			// Plugin Folder Path
+			// Plugin Folder Path.
 			if ( ! defined( 'AFFWP_AAT_PLUGIN_DIR' ) ) {
 				define( 'AFFWP_AAT_PLUGIN_DIR', plugin_dir_path( $this->file ) );
 			}
 
-			// Plugin Folder URL
+			// Plugin Folder URL.
 			if ( ! defined( 'AFFWP_AAT_PLUGIN_URL' ) ) {
 				define( 'AFFWP_AAT_PLUGIN_URL', plugin_dir_url( $this->file ) );
 			}
 
-			// Plugin Root File
+			// Plugin Root File.
 			if ( ! defined( 'AFFWP_AAT_PLUGIN_FILE' ) ) {
 				define( 'AFFWP_AAT_PLUGIN_FILE', $this->file );
 			}
@@ -173,26 +174,26 @@ if ( ! class_exists( 'AffiliateWP_Affiliate_Area_Tabs' ) ) {
 		 */
 		public function load_textdomain() {
 
-			// Set filter for plugin's languages directory
+			// Set filter for plugin's languages directory.
 			$lang_dir = dirname( plugin_basename( $this->file ) ) . '/languages/';
 			$lang_dir = apply_filters( 'affiliatewp_affiliate_area_tabs_languages_directory', $lang_dir );
 
-			// Traditional WordPress plugin locale filter
-			$locale   = apply_filters( 'plugin_locale',  get_locale(), 'affiliatewp-affiliate-area-tabs' );
+			// Traditional WordPress plugin locale filter.
+			$locale   = apply_filters( 'plugin_locale', get_locale(), 'affiliatewp-affiliate-area-tabs' );
 			$mofile   = sprintf( '%1$s-%2$s.mo', 'affiliatewp-affiliate-area-tabs', $locale );
 
-			// Setup paths to current locale file
+			// Setup paths to current locale file.
 			$mofile_local  = $lang_dir . $mofile;
 			$mofile_global = WP_LANG_DIR . '/affiliatewp-affiliate-area-tabs/' . $mofile;
 
 			if ( file_exists( $mofile_global ) ) {
-				// Look in global /wp-content/languages/affiliatewp-affiliate-area-tabs/ folder
+				// Look in global /wp-content/languages/affiliatewp-affiliate-area-tabs/ folder.
 				load_textdomain( 'affiliatewp-affiliate-area-tabs', $mofile_global );
 			} elseif ( file_exists( $mofile_local ) ) {
-				// Look in local /wp-content/plugins/affiliatewp-affiliate-area-tabs/languages/ folder
+				// Look in local /wp-content/plugins/affiliatewp-affiliate-area-tabs/languages/ folder.
 				load_textdomain( 'affiliatewp-affiliate-area-tabs', $mofile_local );
 			} else {
-				// Load the default language files
+				// Load the default language files.
 				load_plugin_textdomain( 'affiliatewp-affiliate-area-tabs', false, $lang_dir );
 			}
 		}
@@ -236,7 +237,7 @@ if ( ! class_exists( 'AffiliateWP_Affiliate_Area_Tabs' ) ) {
 		 */
 		private function hooks() {
 
-			// plugin meta
+			// plugin meta.
 			add_filter( 'plugin_row_meta', array( $this, 'plugin_meta' ), null, 2 );
 
 			// Render the tab content.
@@ -245,7 +246,7 @@ if ( ! class_exists( 'AffiliateWP_Affiliate_Area_Tabs' ) ) {
 			// Redirect if non-affiliate tries to access a tab's page.
 			add_action( 'template_redirect', array( $this, 'redirect' ) );
 
-			// User has at least AffiliateWP version 2.1.7
+			// User has at least AffiliateWP version 2.1.7.
 			if ( $this->has_2_1_7() ) {
 
 				/**
@@ -267,7 +268,7 @@ if ( ! class_exists( 'AffiliateWP_Affiliate_Area_Tabs' ) ) {
 		 * Filter an existing tab's content
 		 *
 		 * @since 1.1.2
-		 * @param string $content
+		 * @param string $content The tab content.
 		 * @param string $active_tab The slug of the active tab.
 		 *
 		 * @return string $content The content to show in the tab.
@@ -293,7 +294,7 @@ if ( ! class_exists( 'AffiliateWP_Affiliate_Area_Tabs' ) ) {
 		 * @since 1.1.2
 		 * @param string $active_tab The slug of the active tab.
 		 *
-		 * @return string $content The content of the tab
+		 * @return void
 		 */
 		public function custom_tab_content( $active_tab ) {
 			?>
@@ -308,6 +309,8 @@ if ( ! class_exists( 'AffiliateWP_Affiliate_Area_Tabs' ) ) {
 		 *
 		 * @since 1.1
 		 *
+		 * @param bool   $show Whether to show the given tab.
+		 * @param string $tab  The given tab slug.
 		 * @return boolean
 		 */
 		public function hide_existing_tabs( $show, $tab ) {
@@ -332,6 +335,7 @@ if ( ! class_exists( 'AffiliateWP_Affiliate_Area_Tabs' ) ) {
 		 *
 		 * @since 1.2
 		 *
+		 * @param array $tabs Array of tabs.
 		 * @return array $tabs The tabs to show in the Affiliate Area
 		 */
 		public function affiliate_area_tabs( $tabs ) {
@@ -348,7 +352,7 @@ if ( ! class_exists( 'AffiliateWP_Affiliate_Area_Tabs' ) ) {
 				foreach ( $affiliate_area_tabs as $key => $tab ) {
 
 					if ( isset( $tab['slug'] ) ) {
-						$new_tabs[$tab['slug']] = $tab['title'];
+						$new_tabs[ $tab['slug'] ] = $tab['title'];
 
 						/**
 						 * If the saved tab slug exists inside $affiliate_area_tabs, but not in $tabs (tabs from filter),
@@ -361,11 +365,10 @@ if ( ! class_exists( 'AffiliateWP_Affiliate_Area_Tabs' ) ) {
 							 * and only visible in the Affiliate Area if the affiliate has access.
 							 */
 							if ( array_key_exists( $tab['slug'], $this->functions->add_on_tabs() ) && ! is_admin() ) {
-								unset( $new_tabs[$tab['slug']] );
+								unset( $new_tabs[ $tab['slug'] ] );
 							} else {
-								unset( $new_tabs[$tab['slug']] );
+								unset( $new_tabs[ $tab['slug'] ] );
 							}
-
 						}
 
 						// Store an array of tab slugs.
@@ -494,22 +497,22 @@ if ( ! class_exists( 'AffiliateWP_Affiliate_Area_Tabs' ) ) {
 		/**
 		 * Modify plugin metalinks.
 		 *
-		 * @access      public
-		 * @since       1.0.0
-		 * @param       array $links The current links array
-		 * @param       string $file A specific plugin table entry
-		 * @return      array $links The modified links array
+		 * @access public
+		 * @since  1.0.0
+		 * @param array  $links The current links array.
+		 * @param string $file A specific plugin table entry.
+		 * @return array $links The modified links array
 		 */
 		public function plugin_meta( $links, $file ) {
-		    if ( $file == plugin_basename( $this->file ) ) {
-		        $plugins_link = array(
-		            '<a title="' . __( 'Get more add-ons for AffiliateWP', 'affiliatewp-affiliate-area-tabs' ) . '" href="'. admin_url( 'admin.php?page=affiliate-wp-add-ons' ) . '">' . __( 'More add-ons', 'affiliatewp-affiliate-area-tabs' ) . '</a>'
-		        );
+			if ( plugin_basename( $this->file ) === $file ) {
+					$plugins_link = array(
+						'<a title="' . __( 'Get more add-ons for AffiliateWP', 'affiliatewp-affiliate-area-tabs' ) . '" href="' . admin_url( 'admin.php?page=affiliate-wp-add-ons' ) . '">' . __( 'More add-ons', 'affiliatewp-affiliate-area-tabs' ) . '</a>',
+					);
 
-		        $links = array_merge( $links, $plugins_link );
-		    }
+					$links = array_merge( $links, $plugins_link );
+			}
 
-		    return $links;
+			return $links;
 		}
 	}
 
