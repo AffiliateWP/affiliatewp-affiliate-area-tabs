b7b3290d228099da27e05c001722189fb0a384f2
dbeja <david.beja@gmail.com>
2021-06-07 10:21:02 +0100

    Recreate main plugin file. #98

---
+++ affiliatewp-affiliate-area-tabs.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: AffiliateWP - Affiliate Area Tabs
+ * Plugin URI: https://affiliatewp.com/add-ons/official-free/affiliate-area-tabs/
+ * Description: Add and reorder tabs in the Affiliate Area
+ * Author: Sandhills Development, LLC
+ * Author URI: https://sandhillsdev.com
+ * Version: 1.2.9
+ * Text Domain: affiliatewp-affiliate-area-tabs
+ * Domain Path: languages
+ *
+ * AffiliateWP is distributed under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * AffiliateWP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AffiliateWP. If not, see <http://www.gnu.org/licenses/>.
+ */
